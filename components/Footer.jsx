f5889958017af4a87ff7238ca3a236f4855af238
Rishabh Singh <ris8645208@gmail.com>
2021-08-16 16:23:21 +0530

    init and added basic pages

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                Copyright 2021 Ninja List
+        </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const About = () => {
+    return ( 
+        <div>
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam inventore, cum natus beatae odit culpa provident molestias! Qui placeat impedit perferendis reprehenderit rerum quidem facere, asperiores inventore voluptatibus eveniet omnis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis modi quos aspernatur magni hic blanditiis? Sed odio a vel debitis! Quis eius in tenetur eaque et, ducimus obcaecati quos modi!</p>
+        </div>
+     );
+}
+ 
+export default About;
